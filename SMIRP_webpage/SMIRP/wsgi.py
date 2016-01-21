131a66ce1c73dd4e1eaee69735e6db3f56a21140
seanrt <seanrtohidi@gmail.com>
2016-01-21 14:21:56 -0500

    First SMIRP commit

--- SMIRP_webpage/SMIRP/wsgi.py
+++ SMIRP_webpage/SMIRP/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for SMIRP_webpage project.
+WSGI config for SMIRP project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SMIRP_webpage.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SMIRP.settings")
 
 application = get_wsgi_application()
